962149f12dc210d021035d2e7fea9854e041a065
Tomáš Fejfar <tomas.fejfar@gmail.com>
2014-10-15 13:46:54 +0100

    wip

--- app/view2/view2.js
+++ app/view2/view2.js
@@ -2,21 +2,27 @@
 
 angular.module('myApp.view2', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
-}])
-
-.controller('View2Ctrl', ['$scope','$http', function($scope, $http) {
-      $scope.master = {};
-      $scope.overlay = false;
-      $scope.displayRepos = function(search) {
-        $scope.overlay = true;
-        $http.get('https://api.github.com/users/' + $scope.search + '/repos').success(function (data) {
-          $scope.repos = data;
-          $scope.overlay = false;
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/view2', {
+            templateUrl: 'view2/view2.html',
+            controller: 'View2Ctrl'
         });
-      };
-}]);
+    }])
+
+    .controller('View2Ctrl', ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
+        $scope.master = {};
+        $scope.overlay = false;
+        $scope.displayRepos = function (search) {
+            $scope.overlay = true;
+            $http.get('https://api.github.com/users/' + search + '/repos').success(function (data) {
+                $scope.repos = data;
+                $scope.overlay = false;
+            });
+        };
+        $scope.displayRepos('tomasfejfar');
+
+        $scope.order = function (predicate, reverse) {
+            var orderBy = $filter('orderBy');
+            $scope.repos = orderBy($scope.repos, predicate, reverse);
+        }
+    }]);
